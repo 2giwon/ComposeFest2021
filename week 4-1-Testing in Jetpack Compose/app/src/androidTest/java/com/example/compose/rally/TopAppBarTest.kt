b4dd075078b126c447437ed6df5add1d0300867e
middlefgr <2giwon@gmail.com>
2021-12-03 00:30:55 +0900

    [codelab-4-1] Optional exercise

--- week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -3,9 +3,11 @@ package com.example.compose.rally
 import androidx.compose.ui.test.hasContentDescription
 import androidx.compose.ui.test.hasParent
 import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.isSelected
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.printToLog
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import org.junit.Rule
@@ -36,5 +38,45 @@ class TopAppBarTest {
             useUnmergedTree = true
         ).assertExists()
 
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentTabSelected() {
+        val allScreen = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreen,
+                onTabSelected = {},
+                currentScreen = RallyScreen.Bills
+            )
+        }
+
+        composeTestRule.onRoot(useUnmergedTree = true).printToLog("currentLabelExists")
+        composeTestRule.onNode(
+            isSelected() and hasContentDescription(RallyScreen.Bills.name),
+            useUnmergedTree = true
+        ).assertExists()
+    }
+
+    @Test
+    fun rallyTopAppBarTest_changeSelectedTab() {
+        val allScreen = RallyScreen.values().toList()
+        var currentTab: RallyScreen = RallyScreen.Overview
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreen,
+                onTabSelected = { screen ->
+                    currentTab = screen
+                },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        for (value in RallyScreen.values()) {
+            composeTestRule
+                .onNodeWithContentDescription(value.name)
+                .performClick()
+            assert(currentTab == value)
+        }
     }
 }
